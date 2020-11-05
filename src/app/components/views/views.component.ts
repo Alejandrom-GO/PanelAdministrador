60247a71b005064adf9031e6371f86311e047055
Alejandrom-GO <miguel.a.gutierrezortega@gmail.com>
2020-11-05 16:39:52 -0600

    Swetalert integracion

--- src/app/components/views/views.component.ts
+++ src/app/components/views/views.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ConexionService } from '../../services/conexion.service';
 import * as $ from 'jquery';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-views',
@@ -8,12 +9,27 @@ import * as $ from 'jquery';
   styleUrls: ['./views.component.css']
 })
 export class ViewsComponent implements OnInit {
+  public mesas: any = [];
+  public mesasDisponible: any = [];
+  public mesaReservada: any = [];
+  public mesaAbierta: any = [];
+  public mesaUsuario: any = [];
 items: any;
 itemEditar: any = {firstname: '' };
   constructor(private con: ConexionService) {
   this.con.retornaItems().subscribe(items => {
     this.items = items;
     // console.log(this.items);
+  });
+  this.con.retornarMesas().subscribe((res: any) => {
+    this.mesas = res;
+    // console.log(this.mesas);
+    for (const mesa of this.mesas) {
+      if (mesa.estado === 'disponible') {
+        this.mesasDisponible.push(mesa);
+      }
+    }
+    // console.log(this.mesasDisponible);
   });
    }
   ngOnInit(): void {
@@ -26,12 +42,25 @@ itemEditar: any = {firstname: '' };
 
   // tslint:disable-next-line:typedef
   editar(item){
-     console.log(item);
      this.itemEditar = item;
+     this.mesaUsuario = this.itemEditar.mesa;
   }
   // tslint:disable-next-line:typedef
   editarform(){
+    this.mesaReservada = {id: this.itemEditar.mesa, numeroMesa: this.itemEditar.mesa, estado: 'Reservado'};
+    if (this.mesaAbierta !== this.itemEditar.mesa){
+      this.mesaAbierta = {id: this.mesaUsuario, numeroMesa: this.mesaUsuario, estado: 'disponible'};
+      this.con.editarmesa(this.mesaAbierta);
+    }
+    this.con.editarmesa(this.mesaReservada);
     this.con.editar(this.itemEditar);
+    Swal.fire({
+      icon: 'success',
+      title: 'Modificacion Exitosa',
+      confirmButtonText: 'OK'
+    }).then(() => {
+      window.location.href = '/';
+    });
   }
   // tslint:disable-next-line:typedef
   cerrar(){
--- src/app/services/conexion.service.ts
+++ src/app/services/conexion.service.ts
@@ -5,18 +5,30 @@ import { map } from 'rxjs/operators';
 import 'firebase/firestore';
 
 export interface Item {firstname: string; }
+export interface Mesas {name: string; }
 @Injectable({
   providedIn: 'root'
 })
 export class ConexionService {
   private itemsCollection: AngularFirestoreCollection<Item>;
   private itemDoc: AngularFirestoreDocument<Item>;
+  private MesaDoc: AngularFirestoreDocument<Mesas>;
   items: Observable<Item[]>;
+  private MesasCollection: AngularFirestoreCollection<Mesas>;
+  mesas: Observable<Mesas[]>;
   constructor(private readonly afs: AngularFirestore) {
     this.itemsCollection = afs.collection<Item>('items');
     this.items = this.itemsCollection.snapshotChanges().pipe(
       map(actions => actions.map(a => {
         const data = a.payload.doc.data() as Item;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    );
+    this.MesasCollection = afs.collection<Mesas>('mesas');
+    this.mesas = this.MesasCollection.snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as Mesas;
         const id = a.payload.doc.id;
         return { id, ...data };
       }))
@@ -28,13 +40,25 @@ export class ConexionService {
     return this.items;
   }
   // tslint:disable-next-line:typedef
+  retornarMesas(){
+    return this.mesas;
+    // return this.afs.collection('mesas').snapshotChanges;
+    // return this.afs.collection('mesas').doc('5nBEvdb8t7pU2MCY5Ntb').snapshotChanges;
+  }
+  // tslint:disable-next-line:typedef
   eliminar(id: any){
     this.itemDoc = this.afs.doc<Item>('items/' + id);
     this.itemDoc.delete();
   }
   // tslint:disable-next-line:typedef
   editar(item){
-    this.itemDoc = this.afs.doc<Item>('items/' + item.id);
-    this.itemDoc.update(item);
+     this.itemDoc = this.afs.doc<Item>('items/' + item.id);
+     this.itemDoc.update(item);
+  }
+
+  // tslint:disable-next-line:typedef
+  editarmesa(item){
+    this.MesaDoc = this.afs.doc<Mesas>('mesas/' + item.id);
+    this.MesaDoc.update(item);
   }
 }
